b4877c441291c8943d642907e19f8eb8c596e421
Andrew Ulrich <andrewfulrich@gmail.com>
2020-04-20 02:03:54 -0500

    made it into a quiz

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Controls, Map, Popup, LayerPanel, loadDataLayer } from '@bayer/ol-kit'
 import olStyle from 'ol/style/style'
 import olStroke from 'ol/style/stroke'
@@ -10,93 +10,60 @@ import olView from 'ol/view'
 import TileLayer from 'ol/layer/tile';
 import BingMaps from 'ol/source/bingmaps';
 import OSM from 'ol/source/osm';
-
-class App extends Component {
-  constructor(props) {
-    super(props)
-
+import BingMap from './BingMap'
+function App() {
+  const [gameStarted, setGameStarted] = useState(false);
+  const [currentName, setCurrentName] = useState('');
+  const [checkingAnswer,setCheckingAnswer] = useState(false);
+  const [isAnswerRight,setIsAnswerRight] = useState(null);
+  let currentAnswer=''
+  document.addEventListener('setName',(e)=>{
+    console.log(e)
+    setCurrentName(e.detail)
+  })
+  function checkAnswer() {
+    setCheckingAnswer(true)
+    setIsAnswerRight(currentName.toLowerCase().replace(/\W/g,'')==currentAnswer.toLowerCase().replace(/\W/g,''))
   }
-  render() {
-    const onMapInit = async map => {
-      console.log('we got a map!', map)
-      window.map = map
-      console.log(map.getLayers())
-      const dataLayer2 = await loadDataLayer(map, 'https://maps6.stlouis-mo.gov/arcgis/rest/services/Hosted/Neighborhood_Boundaries/FeatureServer/0/query?f=geojson&where=1=1&returnGeometry=true&outSR=4326&cacheHint=true')
-
-      const features=dataLayer2.getSource().getFeatures()
-      console.log(features)
-      const tif=features.find(f=>f.get('nhd_name')=='West End')
-      console.log(tif)
-      tif.setStyle(new olStyle({
-        fill: new olFill({
-          color: 'rgba(0, 0, 255, 0.3)'
-        }),
-        stroke: new olStroke({//
-          color: 'black'
-        })
-      }))
-      function zoomToFeature(feature) {
-        const extent=feature.getGeometry().getExtent()
-        const center=olProj.toLonLat(olExtent.getCenter(extent))
-        //centerAndZoom(map,{y:center[1],x:center[0],zoom:16})
-
-        // var extent = my_vector_layer.getSource().getExtent();
-        map.getView().fit(extent, map.getSize());
-      }
-      zoomToFeature(tif)
-      // const request = await fetch('https://maps6.stlouis-mo.gov/arcgis/rest/services/Hosted/Neighborhood_Boundaries/FeatureServer/0/query?f=geojson&where=1=1&returnGeometry=true&outSR=4326&cacheHint=true')
-      // const data = await request.json()
-      // const names=data.features.map(f=>f.properties.nhd_name)
-      // console.log(names)
-
-      console.log('dataLayer', dataLayer2)
-      // centerAndZoom(map,{y:38.622042,x:-90.280927,zoom:12.52})
-    }
-
-    var myStyle = {
-      "elements": {
-          "water": { "fillColor": "#a1e0ff" },
-          "waterPoint": { "iconColor": "#a1e0ff" },
-          "transportation": { "strokeColor": "#aa6de0" },
-          "road": { "fillColor": "#b892db" },
-          "railway": { "strokeColor": "#a495b2" },
-          "structure": { "fillColor": "#ffffff" },
-          "runway": { "fillColor": "#ff7fed" },
-          "area": { "fillColor": "#f39ebd" },
-          "political": { "borderStrokeColor": "#fe6850", "borderOutlineColor": "#55ffff" },
-          "point": { "iconColor": "#ffffff", "fillColor": "#FF6FA0", "strokeColor": "#DB4680" },
-          "transit": { "fillColor": "#AA6DE0" }
-      },
-        "version": "1.0"
-    };
-    const bingOptions={
-      key:'AkmzPc-MgCT7p-2b7GXljWNSVrJRMNnVcNC_WZZrMn7IPoVb5L5VbeiZu8lwcIEQ',
-      customMapStyle: myStyle
-    }
-    const myMap = new olMap({
-      view: new olView({
-        center: [0, 0],
-        zoom: 1
-      }),
-      layers: [
-        new TileLayer({
-          source: new BingMaps({
-            sourceOpts:bingOptions
-          })
-        })
-      ],
-      target: 'map'
+  function startGame() {
+    setGameStarted(true)
+    getNext()
+  }
+  function getNext() {
+    const event=new CustomEvent('getNext',{
+      //no metadata
     })
-
-    return (
-      <Map map={myMap} onMapInit={onMapInit} fullScreen>
-        <Controls />
-        <Popup />
-        <LayerPanel />
-      </Map>
-    )
+    document.dispatchEvent(event)
+    setIsAnswerRight(null)
+    setCheckingAnswer(false)
   }
 
+  function handleChange(e) {
+    currentAnswer=e.target.value
+  }
+  
+  return (
+    <div id="app">
+      <div id="form">
+        <h1>The Lou Quiz</h1>
+        {!gameStarted && <button className="centered" onClick={startGame}>Start</button>}
+        {gameStarted &&
+        <div className="centered question">
+          <label htmlFor="yourAnswer">What is the name of the area shown below?</label><input onChange={handleChange} />{currentName}
+          {checkingAnswer ? 
+            <button onClick={getNext}>Next</button>
+            :
+            <button onClick={checkAnswer}>Submit</button>
+          }
+          {isAnswerRight !== null &&
+            <h3>{isAnswerRight ? `You're correct!`:`Sorry, the answer was ${currentName}`}</h3>
+          }
+        </div>
+        }
+        </div>
+      <BingMap></BingMap>
+    </div>
+  )
 }
 
 export default App
--- src/BingMap.js
+++ src/BingMap.js
@@ -1,6 +1,264 @@
-import { connectToMap } from '@bayer/ol-kit'
-import { BingMaps} from '@bayer/ol-kit'
-function BingMap() {
+import React, { Component } from 'react'
+import { Controls, Map, Popup, LayerPanel, loadDataLayer } from '@bayer/ol-kit'
+import olStyle from 'ol/style/style'
+import olStroke from 'ol/style/stroke'
+import olFill from 'ol/style/fill'
+import olText from 'ol/style/text'
+import olExtent from 'ol/extent'
+import olProj from 'ol/proj';
+import olMap from 'ol/map'
+import olView from 'ol/view'
+import TileLayer from 'ol/layer/tile';
+import BingMaps from 'ol/source/bingmaps';
+import OSM from 'ol/source/osm';
+
+class BingMap extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+  render() {
+    const onMapInit = async map => {
+      console.log('we got a map!', map)
+      window.map = map
+      console.log(map.getLayers())
+      const munies = await loadDataLayer(map,'https://maps.stlouisco.com/arcgis/rest/services/OpenData/OpenData/FeatureServer/6/query?where=1%3D1&outFields=MUNICIPALITY,last_edited_date,GlobalID&outSR=4326&f=geojson')
+      const neighborhoods = await loadDataLayer(map, 'https://maps6.stlouis-mo.gov/arcgis/rest/services/Hosted/Neighborhood_Boundaries/FeatureServer/0/query?f=geojson&where=1=1&returnGeometry=true&outSR=4326&cacheHint=true')
+      
+      // const streetNameLayer=await loadDataLayer(map,'https://maps.stlouisco.com/arcgis/rest/services/OpenData/OpenData/FeatureServer/1/query?where=1%3D1&outFields=FULL_NAME&outSR=4326&f=geojson')
+      // const streetFeatures=streetNameLayer.getSource().getFeatures()
+      // streetFeatures.forEach(sf=>{
+        // sf.setStyle(new olStyle({
+          // text: new Text({
+          //   font: '12px Calibri,sans-serif',
+          //   overflow: true,
+          //   fill: new olFill({
+          //     color: '#000'
+          //   }),
+          //   stroke: new olStroke({
+          //     color: '#fff',
+          //     width: 3
+          //   }),
+          //   text:sf.get('FULL_NAME')
+          // })
+      //   }))
+      // })
+      // console.log('streetFeatures',streetNameLayer)
+      function getFontSize() {
+        return map.getView().getZoom()
+        // switch(true) {
+        //   case  zoom < 11:
+        //     return 8;
+        //   case zoom < 13:
+        //     return 10;
+        //   case zoom < 14:
+        //     return 14;
+        //   case zoom < 16:
+        //     return 16;
+        //   default: return 20;
+        // }
+      }
+      function getMunyText(feature) {
+        return (feature.get('MUNICIPALITY').toLowerCase()+'').split(' ').map(name=>name[0].toUpperCase()+name.substring(1)).join(' ').replace('Unincorporated','')
+      }
+      function showLabel(feature,isNeighborhood) {
+        feature.setStyle(new olStyle({
+          fill: new olFill({
+            color: 'rgba(255, 255, 255, 0.3)'
+          }),
+          stroke: new olStroke({//
+            color: 'blue'
+          }),
+          text:new olText({ // from https://openlayers.org/en/latest/examples/vector-labels.html and https://openlayers.org/en/latest/examples/vector-label-decluttering.html
+            font: `${getFontSize()}px Calibri,sans-serif`,
+            overflow: true,
+            fill: new olFill({
+              color: '#000'
+            }),
+            stroke: new olStroke({
+              color: '#fff',
+              width: 3
+            }),
+            text:isNeighborhood? feature.get('nhd_name') : getMunyText(feature)
+          })
+        }))
+      }
+      function hideLabel(feature) {
+        feature.setStyle(new olStyle({
+          fill: new olFill({
+            color: 'rgba(255, 255, 255, 0.3)'
+          }),
+          stroke: new olStroke({//
+            color: 'black'
+          }),
+          text:new olText({ // from https://openlayers.org/en/latest/examples/vector-labels.html and https://openlayers.org/en/latest/examples/vector-label-decluttering.html
+            font: `${getFontSize()}px Calibri,sans-serif`,
+            overflow: true,
+            fill: new olFill({
+              color: '#000'
+            }),
+            stroke: new olStroke({
+              color: '#fff',
+              width: 3
+            }),
+            text:''
+          })
+        }))
+      }
+      function highlight(feature) {
+        feature.setStyle(new olStyle({
+          fill: new olFill({
+            color: 'rgba(0,0, 255, 0.3)'
+          }),
+          stroke: new olStroke({//
+            color: 'blue'
+          }),
+          text:new olText({ // from https://openlayers.org/en/latest/examples/vector-labels.html and https://openlayers.org/en/latest/examples/vector-label-decluttering.html
+            font: `${getFontSize()}px Calibri,sans-serif`,
+            overflow: true,
+            fill: new olFill({
+              color: '#000'
+            }),
+            stroke: new olStroke({
+              color: '#fff',
+              width: 3
+            }),
+            text:''
+          })
+        }))
+      }
+      const features=neighborhoods.getSource().getFeatures()
+      
+      //console.log(features)
+      const tif=features.find(f=>f.get('nhd_name')=='West End')
+      // //console.log(tif)
+      // tif.setStyle(new olStyle({
+      //   fill: new olFill({
+      //     color: 'rgba(0, 0, 255, 0.3)'
+      //   }),
+      //   stroke: new olStroke({//
+      //     color: 'black'
+      //   }),
+      //   text:new olText({
+      //     font: '20px Calibri,sans-serif',
+      //     overflow: true,
+      //     fill: new olFill({
+      //       color: '#000'
+      //     }),
+      //     stroke: new olStroke({
+      //       color: '#fff',
+      //       width: 3
+      //     }),
+      //     text:tif.get('nhd_name')
+      //   })
+      // }))
+      const munyFeatures=munies.getSource().getFeatures()
+      const irrelevant=munyFeatures.filter(f=>f.get('MUNICIPALITY')=='UNINCORPORATED')
+      const munySource=munies.getSource()
+      irrelevant.forEach(f=>munySource.removeFeature(f))
+      // console.log('irrelevant',irrelevant)
+      // console.log('munyFeature: ',munyFeatures[0])
+      
+      function zoomToFeature(feature) {
+        const extent=feature.getGeometry().getExtent()
+        // const center=olProj.toLonLat(olExtent.getCenter(extent))
+        //centerAndZoom(map,{y:center[1],x:center[0],zoom:16})
+
+        // var extent = my_vector_layer.getSource().getExtent();
+        map.getView().fit(extent, map.getSize());
+        map.getView().setZoom(map.getView().getZoom()-0.7)
+      }
+      /******zoomToFeature(tif)*******/
+      
+      // const request = await fetch('https://maps6.stlouis-mo.gov/arcgis/rest/services/Hosted/Neighborhood_Boundaries/FeatureServer/0/query?f=geojson&where=1=1&returnGeometry=true&outSR=4326&cacheHint=true')
+      // const data = await request.json()
+      // const names=data.features.map(f=>f.properties.nhd_name)
+      // console.log(names)
+      /**************** fit zoom to both layers *******************/
+      const combinedExtent=olExtent.createEmpty()
+      olExtent.extend(combinedExtent, munies.getSource().getExtent());
+      olExtent.extend(combinedExtent, neighborhoods.getSource().getExtent());
+      map.getView().fit(combinedExtent, map.getSize());
+      map.getView().setZoom(map.getView().getZoom()+0.7)
+      /**************** /fit zoom to both layers *******************/
+
+      function showAllLabels() {
+        munyFeatures.forEach(f=>showLabel(f,false))
+        features.forEach(f=>showLabel(f,true))
+      }
+      showAllLabels()
+
+      /**************** get the next random thing *******************/
+      function getNextThing() {
+        munyFeatures.forEach(f=>showLabel(f))
+        features.forEach(f=>showLabel(f,true))
+        const nextIndex=Math.floor(Math.random()*(munyFeatures.length+features.length-1))
+        let nextFeature,isNeighborhood;
+        if(nextIndex < munyFeatures.length) {
+          nextFeature=munyFeatures[nextIndex]
+          isNeighborhood=false; //todo: just add this as a property of the features
+        } else {
+          nextFeature=features[nextIndex-munyFeatures.length]
+          isNeighborhood=true
+        }
+        console.log("next feature: ",nextFeature)
+        highlight(nextFeature,isNeighborhood)
+        zoomToFeature(nextFeature)
+
+        //callback(isNeighborhood ? getMunyText(nextFeature):nextFeature.get('nhd_name'))
+        const event=new CustomEvent('setName',{
+          detail:isNeighborhood? nextFeature.get('nhd_name') : getMunyText(nextFeature)
+        })
+        document.dispatchEvent(event)
+      }
+      document.addEventListener('getNext',()=>{
+        getNextThing()
+      })
+      /**************** /get the next random thing *******************/
+
+      console.log('dataLayer', neighborhoods)
+      // centerAndZoom(map,{y:38.622042,x:-90.280927,zoom:12.52})
+    }
+
+    const bingy=new BingMaps({
+      key: 'YOUR KEY HERE',
+      imagerySet: 'RoadOnDemand',
+      mapLayer:'Background',
+      tileLoadFunction:function(tile, src) {
+        //hide labels
+        tile.getImage().src = src.replace('it=G,L','it=G');
+        // console.log(tile)
+      }
+      // projection: olProj.createProjection('EPSG:4326')
+    })
+    // console.log('proj:',olProj.createProjection('EPSG:4326'))
+    const myMap = new olMap({
+      view: new olView({
+        center: [0, 0],
+        zoom: 1
+      }),
+      layers: [
+        new TileLayer({
+          // customMapStyle: myStyle,
+          opacity:1,
+          visible:true,
+          source: bingy
+        })
+      ],
+      target: 'map'
+    })
+
+    console.log('props:',bingy.getProperties())
+    
+    return (
+
+<Map map={myMap} onMapInit={onMapInit}>
+<Controls />
+      </Map>
+      
+    )
+  }
 
 }
-export default connectToMap(BingMaps)
+
+export default BingMap
